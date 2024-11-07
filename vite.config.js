f98e5f95f83620484f3d2ff888d8f1fc73d24e51
AhmedSail <sdew2sdew0592855602@gmail.com>
2024-11-07 15:16:21 +0200

    finish

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,33 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ["react-redux", "@reduxjs/toolkit", "react-router-dom"],
+//     },
+//   },
+// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000, // لتصبح 1 ميجابايت
     rollupOptions: {
-      external: ["react-redux", "@reduxjs/toolkit", "react-router-dom"],
+      output: {
+        manualChunks: {
+          vendor: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "react-redux",
+            "@reduxjs/toolkit",
+          ],
+        },
+      },
     },
   },
 });
