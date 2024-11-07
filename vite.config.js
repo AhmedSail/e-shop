40b32f9c8d2490a33361ab3ac1cc6a8cd37ef24f
AhmedSail <sdew2sdew0592855602@gmail.com>
2024-11-07 15:32:30 +0200

    finish

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,6 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ["react-redux", "@reduxjs/toolkit", "react-router-dom"],
-//     },
-//   },
-// });
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
   build: {
