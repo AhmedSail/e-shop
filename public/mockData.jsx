b8a786ae82bbd2564a8518589e798a7c56601924
AhmedSail <sdew2sdew0592855602@gmail.com>
2024-11-07 13:29:01 +0200

    finish

---
+++ public/mockData.jsx
@@ -0,0 +1,80 @@
+import Img1 from "../../src/assets/Images/products/074d6b200e4cc3293ffc33ba9b04da1c-removebg-preview.png";
+import Img2 from "../../src/assets/Images/products/664ee578d4c1cd0a1bef150b64ec4263.jpg";
+import Img3 from "../../src/assets/Images/products/880d4b35956e63fdfc958772a131f62d.jpg";
+import Img4 from "../../src/assets/Images/products/92e531875b0405b1609d69aebd169daa.jpg";
+import Img5 from "../../src/assets/Images/products/de935674a817f94fcd0c35ecf8ad933f.jpg";
+import Img6 from "../../src/assets/Images/products/02edf5e7c7423f95e3e10b608cf71bd4.jpg";
+import Img7 from "../../src/assets/Images/products/54c650b88477234ed5e1346a5d56e7f0.jpg";
+import Img8 from "../../src/assets/Images/products/79569a273c82be87adf82bcc9edecc2c.jpg";
+import Img9 from "../../src/assets/Images/products/ace7baf99cff7b2cce99ca5e88a352de-removebg-preview.png";
+import Img10 from "../../src/assets/Images/products/b78ee1a0a8584972c7287ed364d87695.jpg";
+export const Categories = [
+  "Electronics",
+  "Fashion",
+  "Home & kitchen",
+  "Beauty",
+  "Sports",
+  "Automotive",
+];
+export const mockData = [
+  {
+    id: 1,
+    image: Img1,
+    name: "Product 1",
+    price: 29.99,
+  },
+  {
+    id: 2,
+    image: Img2,
+    name: "Product 2",
+    price: 25.99,
+  },
+  {
+    id: 3,
+    image: Img3,
+    name: "Product 3",
+    price: 34.99,
+  },
+  {
+    id: 4,
+    image: Img4,
+    name: "Product 4",
+    price: 52.99,
+  },
+  {
+    id: 5,
+    image: Img5,
+    name: "Product 5",
+    price: 19.99,
+  },
+  {
+    id: 6,
+    image: Img6,
+    name: "Product 6",
+    price: 83.99,
+  },
+  {
+    id: 7,
+    image: Img7,
+    name: "Product 7",
+    price: 76.99,
+  },
+  {
+    id: 8,
+    image: Img8,
+    name: "Product 8",
+    price: 34.05,
+  },
+  {
+    id: 9,
+    image: Img9,
+    name: "Product 9",
+    price: 32.99,
+  },
+  {
+    id: 10,
+    image: Img10,
+    name: "Product 10",
+    price: 12.99,
+  },
+];
